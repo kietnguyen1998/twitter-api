72333405a90737fc65d6b717eb9f53ad97816d98
kietnguyen1998 <nguyenvuanhkiet9191@gmail.com>
2024-10-09 17:04:53 +0700

    uplo2

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,18 @@ import usersRouter from './routes/users.routes'
 import { dafaultErrorHandler } from './middlewares/error.middleware'
 import mediaRouter from './routes/media.routes'
 import { initFolder } from './utils/file'
+import { config } from 'dotenv'
+import argv from 'minimist'
+config()
 databaseService.connect()
 
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000
 
 initFolder()
-
+console.log('arvg', process.argv)
+const options = argv(process.argv.slice(2))
+console.log('options', options.development)
 app.use(express.json())
 app.use('/users', usersRouter)
 app.use('/media', mediaRouter)
--- src/services/media.services.ts
+++ src/services/media.services.ts
@@ -3,13 +3,18 @@ import path from 'path'
 import sharp from 'sharp'
 import { UPLOAD_IMAGE_DIR } from '~/constants/dir'
 import { getNameFromFullName, handleUploadImage } from '~/utils/file'
+import fs from 'fs'
+import { isProduction } from '~/constants/config'
 class MediaService {
   async handleUploadImage(req: Request) {
     const file = await handleUploadImage(req)
     const newName = getNameFromFullName(file.newFilename)
     const newPath = path.resolve(UPLOAD_IMAGE_DIR, `${newName}.jpg`)
     const info = await sharp(file.filepath).jpeg().toFile(newPath)
-    return info
+    fs.unlinkSync(file.filepath)
+    return isProduction
+      ? `${process.env.HOST}/media/${newName}.jpg`
+      : `http://localhost:${process.env.PORT}/media/${newName}.jpg`
   }
 }
 
